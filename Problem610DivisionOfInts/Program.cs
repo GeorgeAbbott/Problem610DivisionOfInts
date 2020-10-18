a88eff4c0b10f117beb3aef3819695cb1b3ce585
George Abbott <georgeabbott314159@outlook.com>
2020-10-18 20:48:43 +0100

    Add project files.

---
+++ Problem610DivisionOfInts/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Problem610DivisionOfInts
+{
+    class Program
+    {
+        static int Divide(int dividend, int divisor)
+        {
+            int count = 0;
+            int timesAdded = 0;
+
+            while (count < dividend)
+            {
+                count += divisor;
+                if ((count <= dividend))
+                    timesAdded++;
+            }
+
+            return timesAdded;
+        }
+
+        static void Main(string[] args)
+        {
+            // Implement division of two positive integers without using the division,
+            // multiplication, or modulus operators. Return the quotient as an integer,
+            // ignoring the remainder.
+
+            int dividend;
+            int divisor;
+
+            while (true)
+            {
+                Console.Write("Enter dividend: ");
+                dividend = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Enter divisor: ");
+                divisor = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Normal Division => {dividend} / {divisor} = {dividend / divisor}");
+                Console.WriteLine($"Divide({dividend}, {divisor}) => {Divide(dividend, divisor)}");
+            }
+
+
+        }
+    }
+}
